a82e6f3991e1860465105270e2bee8925e3f472c
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2023-07-04 17:56:48 -0300

    add inclusão de dados ao localStorage

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import * as C from "./style";
 
-const Form = () => {
+const Form = ({ handleAdd }) => {
     const [desc, setDesc] = useState("");
     const [amount, setAmount] = useState("");
     const [isExpense, setExpense] = useState(false);
+
+    const generateID = () => Math.round(Math.random() * 1000);
 
     const handleSave = () => {
         if (!desc || !amount) {
@@ -14,6 +16,19 @@ const Form = () => {
             alert("O valor tem que ser positivo!");
             return;
         }
+
+        const transaction = {
+            id: generateID(),
+            desc: desc,
+            amount: amount,
+            expense: isExpense,
+        };
+
+        handleAdd(transaction);
+
+        setDesc("");
+        setAmount("");
+
     };
 
     return (
@@ -46,7 +61,7 @@ const Form = () => {
                     <C.Input
                         type="radio"
                         id="rExpanse"
-                        name="group2"
+                        name="group1"
                         onChange={() => setExpense(!isExpense)}
                     />
                     <C.Label htmlFor="rExpense">Saída</C.Label>
